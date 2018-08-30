5621fc1c4e9219277ccbd1e73fa551c429135fc8
nitvit610 <nitvit610@gmail.com>
2018-08-30 16:31:28 +0100

    Changing authentication method
    Receiving MSOnline auth error when trying to connect

--- ExchangeConnect.ps1
+++ ExchangeConnect.ps1
@@ -1,14 +1,15 @@
 $AdminName = "RESIDENTIAL\woodso"
 
 $Pass = Get-Content "C:\Users\woodso\Documents\cred.txt"
+$SecurePass = ConvertTo-SecureString -String $Pass -AsPlainText -Force
 
-$Cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AdminName, $Pass
+$Cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AdminName, $SecurePass
 
 Import-Module MSOnline
 
 Connect-MsolService -Credential $Cred
 
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://be.co.uk/powershell -Authentication Kerberos -Credential $Cred
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://rspex01.willmottresidential.co.uk/powershell/ -Authentication Kerberos -Credential $Cred
 
 Import-PSSession $Session -DisableNameChecking
 
